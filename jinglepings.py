9ab093e42f2cf5b2056d6a4b03839164cab81907
bernardsmith0892 <bernard.smith0892@gmail.com>
2018-12-22 17:26:43 -0500

    Update jinglepings.py

--- jinglepings.py
+++ jinglepings.py
@@ -6,8 +6,10 @@ from PIL import Image
 parser = argparse.ArgumentParser(description='Converts an image file to a list of ping commands to pipe into parallel.')
 
 parser.add_argument('image_file', type=str, help='The image file to generate commands for.')
-parser.add_argument('--xy', type=int, nargs=2, metavar=('x', 'y'), default=(0, 0), help='Where to display the image on the LED wall. The origin is from the top-left of the image.')
-parser.add_argument('--size', type=int, nargs=2, metavar=('width', 'height'), help='Resize the image to the given width and height.')
+parser.add_argument('--xy', type=int, nargs=2, metavar=('X', 'Y'), default=(0, 0), help='Where to display the image on the LED wall. The origin is from the top-left of the image.')
+parser.add_argument('--size', '-s', type=int, nargs=2, metavar=('WIDTH', 'HEIGHT'), help='Resize the image to the given width and height.')
+parser.add_argument('--cmd', '-c', type=str, default='ping -6 -c 4', help="What command to use with the IPv6 addresses. Defaults to 'ping -6 -c 4'.")
+
 
 args = parser.parse_args()
 
@@ -30,7 +32,7 @@ except:
 	print "Error determining origin coordinates."
 	exit()
 
-# Resizes the image to provided width and height. Defaults to original size.
+# Resizes the image to provided width and height. Defaults to image's actual size.
 if args.size != None:
 	try:
 		size = args.size[0], args.size[1]
@@ -47,7 +49,7 @@ for pixel in im.getdata():
 	green = pixel[1]
 	blue = pixel[2]
 
-	command = 'ping -6 -i 0.1 -W 1 2001:4c08:2028:' + str(x) + ':' + str(y) + ':' + hex(red)[2:] + ':' + hex(green)[2:] + ':' + hex(blue)[2:]
+	command = args.cmd + ' 2001:4c08:2028:' + str(x) + ':' + str(y) + ':' + hex(red)[2:] + ':' + hex(green)[2:] + ':' + hex(blue)[2:]
 	print command
 	
 	x = x + 1
@@ -56,5 +58,3 @@ for pixel in im.getdata():
 	if x > x_orig + size[0]:
 		x = x_orig
 		y = y + 1
-
-
